9e255d4c92ff1323f5ca15844e73d98ee9d1c572
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-12-20 17:25:37 +0100

    Stage 2/8: 2+2+

---
+++ calculator/Main.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit){
+            var line = scanner.nextLine();
+            exit = line.equalsIgnoreCase("/exit");
+            if (exit) {
+                System.out.println("Bye!");
+            } else if (!line.isEmpty()){
+                var operands = Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt);
+                System.out.println(operands.sum());
+            }
+        }
+    }
+}
