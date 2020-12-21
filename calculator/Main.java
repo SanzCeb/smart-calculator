cd04f4bb7cc108d7dbb8e3f2e1cc23d6c294d299
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-12-21 09:12:56 +0100

    Stage 4/8: Add subtractions

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,7 +1,8 @@
 package calculator;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+
 
 public class Main {
 
@@ -14,10 +15,24 @@ public class Main {
             if (exit) {
                 System.out.println("Bye!");
             } else if (line.equalsIgnoreCase("/help")){
-                System.out.println("The program calculates the sum of numbers");
+                System.out.println("The program calculates the sum and subtraction of n numbers in a single line");
             } else if (!line.isEmpty()){
-                var operands = Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt);
-                System.out.println(operands.sum());
+                var operands = new HashSet<Integer>();
+                var tokens = line.split("\\s+");
+                boolean neg = false;
+                for (var token : tokens) {
+                    if (token.matches("-?\\d+")) {
+                        var operand = Integer.parseInt(token);
+                        if (neg) {
+                            operand = Math.negateExact(operand);
+                            neg = false;
+                        }
+                        operands.add(operand);
+                    } else if (token.matches("-+")){
+                        neg = token.length() % 2 == 1;
+                    }
+                }
+                System.out.println(operands.stream().mapToInt(Integer::intValue).sum());
             }
         }
     }
